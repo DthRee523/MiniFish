a0551484077b02e3704a7bf84bb6b5e25f3f0ee6
DthRee523 <l928787694@gmail.com>
2022-03-28 02:21:13 +0800

    set text

--- minifish/loginwindow.cpp
+++ minifish/loginwindow.cpp
@@ -6,7 +6,7 @@ LoginWindow::LoginWindow(QWidget *parent)
     , ui(new Ui::LoginWindow)
 {
     ui->setupUi(this);
-    this->setWindowTitle("MiniFish By:DthRee");
+    this->setWindowTitle("MiniFish By:DthRee");//Title
     this->resize(1200, 800);//set Window Size
 }
 
